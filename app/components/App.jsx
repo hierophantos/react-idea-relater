04fcb490c6497058f7050e2f8853ba590758a8e3
Hierophantos <silvergodofgold@gmail.com>
2016-04-26 04:27:33 -0700

    all the thingsss

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'underscore'
 import TttCell from './TttCell.jsx'
 
 export default class App extends React.Component {
@@ -26,36 +27,36 @@ export default class App extends React.Component {
       this.setState ({turn: "X"})
   }
 
+
+
   render() {
+    let tttCell = () => <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
+
     return (
       <div>
-        <h1>Play Ye Some Ticky-Taky!</h1>
-        <p>Player {this.state.turn}'s turn</p>
-        <button onClick={this.nextTurn} >{this.state.turn}</button>
-        <br/>
-        <div><TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
-          <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
-          <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
+        <h1>Play Ye Some Ticky-Tacky!</h1>
+        <p>Player <button >{this.state.turn}</button>'s turn!</p>
+        <div>{_.times (3, () =>
+          <div>
+            {_.times(3, tttCell)}
+          </div>
+         )}
         </div>
-        <div>
-          <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
-          <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
-          <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
-        </div>
-        <div>
-          <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
-          <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
-          <TttCell nextTurn={this.nextTurn} turn={this.state.turn} />
-        </div>
+
+        <hr/>
+        <p>Here be a Counter:</p>
         <button
             onClick={this.increment}
-        >+
+        >
+          +
         </button>
         {this.state.counter}
         <br/><br/>
       </div>
     )
-
   }
-
 }
+
+
+// TODO: Homework: make it so that you can't overwrite a play
+// TODO: Check for Victory Conditions
